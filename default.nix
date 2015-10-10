0e90f6a8034b5a3f54130b69799e8a7c225d9b64
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-10-09 21:29:41 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, array, base, bytestring, containers, directory
+, filepath, ghc-paths, haskeline, knob, mtl, parsec, process
+, stdenv, time, transformers, utf8-string
+}:
+mkDerivation {
+  pname = "husk-scheme";
+  version = "3.19.2";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    array base bytestring containers directory filepath ghc-paths
+    haskeline knob mtl parsec process time transformers utf8-string
+  ];
+  homepage = "http://justinethier.github.com/husk-scheme";
+  description = "R5RS Scheme interpreter, compiler, and library";
+  license = stdenv.lib.licenses.mit;
+}
